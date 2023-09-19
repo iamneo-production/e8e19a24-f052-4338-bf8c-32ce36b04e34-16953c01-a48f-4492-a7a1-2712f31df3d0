7be95e63d7ac9326d32353892defe1a88b9f30fc
demo-college <d15784889@gmail.com>
2023-09-19 10:26:43 +0000

    commit

---
+++ angular/index.js
@@ -0,0 +1,841 @@
+console.log("DFSAfdas")
+console.log("DFSAfdas")
+console.log("DFSAfdas")
+console.log("DFSAfdas")
+console.log("DFSAfdas")
+console.log("DFSAfdas")
+
+
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+
+
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
+for(let i = 0; i <= 100; i++) {
+    console.log("apps")
+}
